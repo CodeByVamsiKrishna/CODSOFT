c0ab6970a968aa17bc9b60b114d911659cb58e5e
Guttula Vamsi Krishna <vamsikish167@gmail.com>
2026-06-22 15:14:36 +0530

    Update To_Do_List_task_1.py

--- To_Do_List_task_1.py
+++ To_Do_List_task_1.py
@@ -1,117 +1,64 @@
-available_options = [
-    "View Task",
-    "Add Task",
-    "Delete Task",
-    "Task Status & Update",
-    "Search Task",
-    "Exit"
-]
+import tkinter as toolkit
+from tkinter import messagebox
+window=toolkit.Tk()
 
-available_task = [
-    "Wake up at morning",
-    "Learn coding",
-    "Practice everything learned",
-    "Don't waste the time"
-]
+def list_boxitems():
+    item=text_input.get()
+    if item !="":
+        list_box.insert(toolkit.END,f"{item} ---- Pending ")
+        text_input.delete(0,toolkit.END)
+        text_input.focus()
 
-task_status = [
-    "pending",
-    "pending",
-    "pending",
-    "pending"
-]
+def mark_buttonuse():
+    if not list_box.curselection():
+        messagebox.showinfo("Info","Select a task first!")
+        return
+    index=list_box.curselection()[0]
+    item2=list_box.get(index)
+    new_item=item2.replace("Pending","Completed")
+    list_box.delete(index)
+    list_box.insert(index,new_item)
 
-while True:
-
-    count = 0
-    for task in available_options:
-        count += 1
-        print(count, task)
-
-    task_number = int(input("Enter the task number to perform task : "))
-
-    if task_number == 1:
-        for task in available_task:
-            print(task)
-
-    elif task_number == 2:
-        new_task = input("Enter the new task to add : ")
-        available_task.append(new_task)
-        task_status.append("pending")
-        print("New task added successfully")
-
-    elif task_number == 3:
-
-        if len(available_task) == 0:
-            print("No tasks available to delete.")
-            continue
-
-        count = 0
-        for task in available_task:
-            count += 1
-            print(count, task)
-
-        to_delete_task = int(input("Enter task number to delete : "))
-        to_delete_task -= 1
-
-        if to_delete_task < 0 or to_delete_task >= len(available_task):
-            print("Invalid task number")
-            continue
-
-        available_task.remove(available_task[to_delete_task])
-        task_status.remove(task_status[to_delete_task])
-
-        print("Removed successfully")
-
-        count = 0
-        for task in available_task:
-            count += 1
-            print(count, task)
-
-    elif task_number == 4:
-
-        if len(available_task) == 0:
-            print("No tasks available.")
-            continue
-
-        count = count2 = 0
-
-        for task in available_task:
-            print(count + 1, task, " - ", task_status[count])
-            count += 1
-
-        completed_task = int(input("Enter task number to mark as completed : "))
-        completed_task -= 1
-
-        if completed_task < 0 or completed_task >= len(task_status):
-            print("Invalid task number")
-            continue
-
-        task_status[completed_task] = "completed"
-
-        print("Successfully marked as completed")
-
-        for task in available_task:
-            print(count2 + 1, available_task[count2], "-", task_status[count2])
-            count2 += 1
-
-    elif task_number == 5:
-        count = 0
-
-        find_task = input("Enter task name to search : ")
-
-        for task in available_task:
-            if task.lower() == find_task.lower():
-                print("Task found")
-                count += 1
-                break
-
-        if count == 0:
-            print("Task not found")
-
-    elif task_number == 6:
-        print("Thank you for using Task Manager. Goodbye!")
-        break
-
+def delete_buttonuse():
+    if list_box.size() >0:
+       list_box.delete(0,toolkit.END)
+       messagebox.showinfo(" Info "," Deleted Succesfully! ")
     else:
-        print("Invalid choice")
+        messagebox.showinfo(" Info "," No Task Exists ")
+
+def delete_itembuttonuse():
+    if not list_box.curselection():
+        messagebox.showinfo("Info","Select Task First")
+        return
+    index = list_box.curselection()[0]
+    list_box.delete(index)
+
+window.title("ToDoList-Application")
+window.geometry("500x600")
+text_label=toolkit.Label(window,text=" Basic ToDoList Application ",font=("Georgia",16))
+text_label.pack(padx=20,pady=5)
+text_input=toolkit.Entry(window,width=40)
+text_input.pack(padx=20,pady=5)
+add_button=toolkit.Button(window,text="  Add Task  ",command=list_boxitems)
+add_button.pack(padx=20,pady=5)
+list_box=toolkit.Listbox(window,width=60,height=15,font=("Arial",12),activestyle="none")
+list_box.pack(pady=15,padx=5)
+
+button_frame=toolkit.Frame(window)
+button_frame.pack()
+
+mark_button=toolkit.Button(button_frame,text=" Mark Complete ",command=mark_buttonuse)
+mark_button.grid(row=0,column=0)
+
+delete_button=toolkit.Button(button_frame,text=" Delete All ",command=delete_buttonuse)
+delete_button.grid(row=0,column=1)
+
+delete_itembutton=toolkit.Button(button_frame,text=" Delete ",command=delete_itembuttonuse)
+delete_itembutton.grid(row=0,column=2)
+
+exit_button=toolkit.Button(button_frame,text=" Exit ",command=window.destroy)
+exit_button.grid(row=0,column=3)
+
+
+
+window.mainloop()
